85acd0231a279a5a8d42390870b5b3eb0cc0af28
Chris Ohk <utilforever@gmail.com>
2026-07-10 20:00:57 +0900

    build: replace setup subprocess calls

--- setup.py
+++ setup.py
@@ -1,83 +1,70 @@
 # Original code from https://github.com/pybind/cmake_example/blob/master/setup.py
 
 import os
-import re
 import sys
 import platform
-import subprocess
 import multiprocessing
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
-from distutils.version import LooseVersion
+
 
 class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
+    def __init__(self, name, sourcedir=""):
+        """Initialize a CMake-backed extension."""
         Extension.__init__(self, name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
 
+
 class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError("CMake must be installed to build the following extensions: " +
-                               ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(
-                re.search(r'version\s*([\d.]+)', out.decode()).group(1))
-            if cmake_version < '3.8.2':
-                raise RuntimeError("CMake >= 3.8.2 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
     def build_extension(self, ext):
-        extdir = os.path.abspath(os.path.dirname(
-            self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable,
-                      '-DBUILD_FROM_PIP=ON']
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir,
+            "-DPYTHON_EXECUTABLE=" + sys.executable,
+            "-DBUILD_FROM_PIP=ON",
+        ]
 
-        env = os.environ.copy()
-        tasking_sys = env.get('TASKING_SYSTEM', '')
+        tasking_sys = os.environ.get("TASKING_SYSTEM", "")
         if tasking_sys:
-            cmake_args += ['-DCUBBYFLOW_TASKING_SYSTEM=' + tasking_sys]
+            cmake_args += ["-DCUBBYFLOW_TASKING_SYSTEM=" + tasking_sys]
 
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
+        cfg = "Debug" if self.debug else "Release"
+        build_args = [
+            "--config",
+            cfg,
+            "--target",
+            "pyCubbyFlow",
+            "--parallel",
+            str(os.environ.get("NUM_JOBS", multiprocessing.cpu_count())),
+        ]
 
         if platform.system() == "Windows":
             cmake_args += [
-                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+                "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+            ]
             if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
+                cmake_args += ["-A", "x64"]
         else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            num_jobs = env.get('NUM_JOBS', multiprocessing.cpu_count())
-            build_args += ['--', '-j%s' % str(num_jobs), 'pyCubbyFlow']
+            cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
 
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
-                                                              self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] +
-                              cmake_args, cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] +
-                              build_args, cwd=self.build_temp)
+        os.environ["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            os.environ.get("CXXFLAGS", ""), self.distribution.get_version()
+        )
+        self.spawn(
+            ["cmake", "-S", ext.sourcedir, "-B", self.build_temp] + cmake_args
+        )
+        self.spawn(["cmake", "--build", self.build_temp] + build_args)
 
 
 setup(
-    name='pyCubbyFlow',
-    version='0.7',
-    author='Chris Ohk',
-    author_email='utilforever@gmail.com',
-    description='Voxel-based fluid simulation engine for computer games',
-    long_description='',
-    ext_modules=[CMakeExtension('pyCubbyFlow')],
-    
+    name="pyCubbyFlow",
+    version="0.7",
+    author="Chris Ohk",
+    author_email="utilforever@gmail.com",
+    description="Voxel-based fluid simulation engine for computer games",
+    long_description="",
+    ext_modules=[CMakeExtension("pyCubbyFlow")],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
 )
