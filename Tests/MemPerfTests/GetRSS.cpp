c99c0af275bc47840507ec16c387ccd017512af1
Chris Ohk <utilforever@gmail.com>
2026-07-28 20:25:52 +0900

    fix: restore Windows memory test build

--- Tests/MemPerfTests/GetRSS.cpp
+++ Tests/MemPerfTests/GetRSS.cpp
@@ -6,8 +6,9 @@
  */
 
 #if defined(_WIN32)
-#include <psapi.h>
 #include <windows.h>
+
+#include <psapi.h>
 
 #elif defined(__unix__) || defined(__unix) || defined(unix) || \
     (defined(__APPLE__) && defined(__MACH__))
